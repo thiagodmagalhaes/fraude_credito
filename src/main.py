3cd613b2f78a0ce01a19c5747bcfa9e97c571dad
Thiago - Engenheiro de dados <thiago.dmagalhaes@hotmail.com>
2025-09-18 17:23:51 -0300

    criar-funcao-leitura: foi criado função quer ler o arquivo .CSV com header e schema das colunas

--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ from pyspark.sql import SparkSession
 
 def criar_sessao_spark():
     '''
-    Essa função cria a sessão do spark e retorna ela.
-    Ela define os configurações do pyspark
+    Define as configurações do pyspark
+        Arg:
+            Não recebe argumentos
+        Return:
+            spark (retorna sessão spark para ser utilizada)
     '''
 
     try:
@@ -20,11 +23,33 @@ def criar_sessao_spark():
         raise e
     
 
+def ler_arquivo_csv(spark, diretorio):
+    '''
+    Realiza a leitura do arquivo csv e retorna em um dataframe
+        Arg: 
+            spark, diretorio  (recebe a sessão spark e o diretório do arquivo csv como argumento)
+        Return: 
+            df (retorna o .csv em um dataframe)
+    '''
+    try:
+        print("Realizando leitura da base .CSV")
+        df = spark.read.csv(diretorio, header=True, inferSchema=True)
+        print("Leitura realizada com sucesso!!!")
+        return df
+    
+    except Exception as e:
+        print(f"Erro ao realizar letura do arquivo CSV: {e}")
+        raise e
+
+
 
 def main():
     try:
         print("Iniciando execução do código. . .")
         spark = criar_sessao_spark()
+        diretorio = rf"C:\Users\Abrasel Nacional\Desktop\TESTE\fraude_credito\data\input\df_fraud_credit.csv"
+        df = ler_arquivo_csv(spark, diretorio)
+        df.show(10, truncate=False)
 
     except Exception as e:
         print(f"EXECUÇÕO INTERROMPIDA!!!  Erro na execução do código: {e}")
